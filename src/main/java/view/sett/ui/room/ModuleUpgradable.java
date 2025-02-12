12cc78f10d9bfba14d6441bf3590e65bf5b2ac62
Emi-the-witch <zennokyrie@gmail.com>
2025-02-12 02:36:29 -0800

    0.4.7 Bug fix for V68.24 since Jake modified RESOURCES file (and added haulers like I had, so the file was removed from the mod). I had to remove the room degrade (ModuleUpgradable) file until I can figure out the new functions Jake made.
    New Feature: Annual average population changes are shown below daily ones. It has detected bugs though, so it may cause issues when Jake fixes it.

--- src/main/java/view/sett/ui/room/ModuleUpgradable.java
+++ src/main/java/view/sett/ui/room/ModuleUpgradable.java
@@ -1,20 +1,26 @@
 package view.sett.ui.room;
 
+import java.util.Arrays;
+
 import game.faction.FACTIONS;
 import game.faction.FResources.RTYPE;
-import game.time.TIME;
+import init.resources.RESOURCE;
 import init.settings.S;
 import init.sprite.SPRITES;
 import init.sprite.UI.Icon;
 import init.sprite.UI.UI;
 import init.text.D;
 import settlement.main.SETT;
-import settlement.maintenance.ROOM_DEGRADER;
-import settlement.room.main.*;
+import settlement.room.main.Room;
+import settlement.room.main.RoomBlueprint;
+import settlement.room.main.RoomBlueprintImp;
+import settlement.room.main.RoomBlueprintIns;
+import settlement.room.main.RoomInstance;
 import settlement.room.service.module.ROOM_SERVICER;
 import snake2d.SPRITE_RENDERER;
 import snake2d.util.color.COLOR;
 import snake2d.util.color.ColorImp;
+import snake2d.util.datatypes.COORDINATE;
 import snake2d.util.datatypes.DIR;
 import snake2d.util.gui.GUI_BOX;
 import snake2d.util.gui.GuiSection;
@@ -24,15 +30,15 @@ import snake2d.util.sprite.text.Str;
 import util.colors.GCOLOR;
 import util.data.GETTER;
 import util.dic.Dic;
-import util.gui.misc.*;
+import util.gui.misc.GBox;
+import util.gui.misc.GButt;
+import util.gui.misc.GGrid;
+import util.gui.misc.GStat;
+import util.gui.misc.GText;
 import util.gui.table.GTableSorter.GTFilter;
 import util.gui.table.GTableSorter.GTSort;
 import util.info.GFORMAT;
 import view.sett.ui.room.Modules.ModuleMaker;
-
-import java.util.Arrays;
-
-import static java.lang.Math.ceil;
 
 final class ModuleUpgradable implements ModuleMaker {
 
@@ -41,13 +47,13 @@ final class ModuleUpgradable implements ModuleMaker {
 	private final CharSequence ¤¤UPGRADABLE = "¤Upgradable:";
 	private final CharSequence ¤¤UPGRADE = "¤Upgrade";
 	private final CharSequence ¤¤UPGRADE_MAX_REACHED = "¤Maximally Upgraded.";
-	//#!# Add new variable
-	public double upgrade_cost = 0;
-	public double maint_cost = 0;
+
+	private final CharSequence ¤¤DOWNGRADED = "¤Downgrade this room. All resources will be lost.";
 
 	ModuleUpgradable(Init init){
 		D.t(this);
 	}
+
 
 	@Override
 	public void make(RoomBlueprint p, LISTE<UIRoomModule> l) {
@@ -57,75 +63,73 @@ final class ModuleUpgradable implements ModuleMaker {
 				l.add(new I(pi));
 			}
 		}
-		
+
 	}
-	
+
 	private class I extends UIRoomModule {
-		
+
 		private final RoomBlueprintIns<?> blueprint;
-		
+
 		I(RoomBlueprintIns<?> blue){
 			this.blueprint = blue;
 		}
-		
+
 		@Override
 		public void appendManageScr(GGrid grid, GGrid text, GuiSection sExta) {
-			
-			
-			
+
+
+
 		}
 
 		@Override
 		public void appendTableFilters(LISTE<GTFilter<RoomInstance>> filters, LISTE<GTSort<RoomInstance>> sorts,
-				LISTE<UIRoomBulkApplier> appliers) {
+					       LISTE<UIRoomBulkApplier> appliers) {
 
 			appliers.add(new UIRoomBulkApplier(new Str(¤¤UPGRADE_ALL)) {
-				
+
 				private final int[] res = new int[16];
-				
+
 				@Override
 				protected void apply(RoomInstance ii) {
-					
+
 					if (ii.upgrade() >= blueprint.upgrades().max() || !ii.blueprintI().upgrades().requires(ii.upgrade()+1).passes(FACTIONS.player()))
 						return;
 					boolean okk = true;
 					for (int ri = 0; ri < blueprint.constructor().resources(); ri++) {
 						int am = ii.resAmount(ri, ii.upgrade()+1) - ii.resAmount(ri, ii.upgrade());
 						okk &= SETT.ROOMS().STOCKPILE.tally().amountReservable.get(blueprint.constructor().resource(ri)) >= am;
-						
+
 					}
 					if (okk) {
 						for (int ri = 0; ri < blueprint.constructor().resources(); ri++) {
 							int am = ii.resAmount(ri, ii.upgrade()+1) - ii.resAmount(ri, ii.upgrade());
-							SETT.ROOMS().STOCKPILE.remove(blueprint.constructor().resource(ri), am, RTYPE.CONSTRUCTION);
+							blueprint.constructor().resource(ri).remove(am, RTYPE.CONSTRUCTION);
 						}
 						ii.upgradeSet(ii.upgrade()+1);
 					}
 				}
-				
+
 				@Override
 				protected void hover(GBox b) {
 					b.title(¤¤UPGRADE_ALL);
 					b.text(¤¤UPGRADE_ALL_DESC);
 					b.NL(8);
-					
+
 					Arrays.fill(res, 0);
-					
+
 					int ok = 0;
 					for (int i = 0; i < blueprint.instancesSize(); i++) {
 						RoomInstance ii = blueprint.getInstance(i);
-
 
 						if (ii.upgrade() < blueprint.upgrades().max() && ii.blueprintI().upgrades().requires(ii.upgrade()+1).passes(FACTIONS.player())) {
 							boolean okk = true;
 							for (int ri = 0; ri < blueprint.constructor().resources(); ri++) {
 								int am = ii.resAmount(ri, ii.upgrade()+1) - ii.resAmount(ri, ii.upgrade());
 								if (SETT.ROOMS().STOCKPILE.tally().amountReservable.get(blueprint.constructor().resource(ri)) >= res[ri] + am) {
-									
+
 								}else {
 									okk = false;
 								}
-
 
 							}
 							if (okk) {
@@ -136,9 +140,9 @@ final class ModuleUpgradable implements ModuleMaker {
 								}
 							}
 						}
-						
+
 					}
-					
+
 					for (int ri = 0; ri < blueprint.constructor().resources(); ri++) {
 						if (res[ri] > 0) {
 							b.add(blueprint.constructor().resource(ri).icon());
@@ -146,17 +150,17 @@ final class ModuleUpgradable implements ModuleMaker {
 							b.NL();
 						}
 					}
-					
+
 					b.NL(8);
-					
+
 					b.textL(¤¤UPGRADABLE);
 					b.add(GFORMAT.iofkInv(b.text(), ok, blueprint.instancesSize()));
 
-					
+
 				}
-				
+
 			});
-			
+
 		}
 
 		@Override
@@ -164,7 +168,7 @@ final class ModuleUpgradable implements ModuleMaker {
 
 			if (blueprint.upgrades().max() > 0) {
 				SPRITE sp = new SPRITE.Imp(Icon.S+Icon.S, Icon.S) {
-					
+
 					@Override
 					public void render(SPRITE_RENDERER r, int X1, int X2, int Y1, int Y2) {
 						RoomInstance ins = get.get();
@@ -180,10 +184,10 @@ final class ModuleUpgradable implements ModuleMaker {
 					}
 				};
 				s.addRightC(16, sp);
-				
+
 			}
 
-			
+
 		}
 
 		@Override
@@ -199,22 +203,38 @@ final class ModuleUpgradable implements ModuleMaker {
 
 		@Override
 		public void appendPanel(GuiSection section, GETTER<RoomInstance> get, int x1, int y1) {
-			
+
+			GButt bb = new GButt.ButtPanel(UI.icons().m.arrow_down) {
+
+				@Override
+				public void hoverInfoGet(GUI_BOX text) {
+					text.text(¤¤DOWNGRADED);
+				}
+
+				@Override
+				protected void clickA() {
+					downGrade(g(get));
+				}
+
+				@Override
+				protected void renAction() {
+					activeSet(g(get).upgrade() > 0);
+				}
+
+			};
+
 			GButt.BSection b = new GButt.BSection() {
 				@Override
 				public void hoverInfoGet(GUI_BOX text) {
-					///#!# reset value
-					upgrade_cost = 0;
-					maint_cost = 0;
 					GBox b = (GBox) text;
 					text.title(¤¤UPGRADE);
-					
+
 					RoomInstance i = get.get();
 					if (i.upgrade() == blueprint.upgrades().max()) {
 						text.text(¤¤UPGRADE_MAX_REACHED);
 						return;
 					}
-					
+
 					if (blueprint.upgrades().desc(g(get).upgrade()+1) != null) {
 						b.text(blueprint.upgrades().desc(g(get).upgrade()+1));
 					}else if (i instanceof ROOM_SERVICER) {
@@ -225,16 +245,14 @@ final class ModuleUpgradable implements ModuleMaker {
 						b.textLL(Dic.¤¤Boosts);
 						b.add(GFORMAT.f0(b.text(), blueprint.upgrades().boost(g(get).upgrade()+1)-blueprint.upgrades().boost(g(get).upgrade())));
 					}
-					
+
 					b.NL(8);
-					
+
 					g(get).blueprintI().upgrades().requires(g(get).upgrade()+1).hover(text, FACTIONS.player());
-					
+
 					b.NL(8);
-					
+
 					for (int ri = 0; ri < blueprint.constructor().resources(); ri++) {
-
-
 						int am =  g(get).resAmount(ri, g(get).upgrade()+1)-g(get).resAmount(ri, g(get).upgrade());
 						if (am > 0) {
 							b.add(blueprint.constructor().resource(ri).icon());
@@ -245,32 +263,12 @@ final class ModuleUpgradable implements ModuleMaker {
 							else
 								t.normalify();
 							b.add(t);
-///////////////////////////////////////////////////////////////////////////////////////////////////////#!#
-							//b.NL();
-
-							//#!# Add denari cost per item
-							RoomInstance ins = get.get();
-							ROOM_DEGRADER deg = get.get().degrader(get.get().mX(), get.get().mY());
-							double iso = ins.isolation(get.get().mX(), get.get().mY());
-							double boost = SETT.MAINTENANCE().speed();
-							double daily_res_use =  ROOM_DEGRADER.rateResource(boost, deg.base(), iso, am);
-							double resource_price = FACTIONS.player().trade.pricesBuy.get(blueprint.constructor().resource(ri));
-							b.text("("+  (int)   ( am * resource_price ) + " denari)");
 							b.NL();
-							//#!# Add new variable for the sum of costs
-							upgrade_cost +=             am * resource_price;
-							maint_cost   +=  daily_res_use * resource_price;
-
 						}
 					}
-					b.text("Estimated total denari cost: " + (int) upgrade_cost);
-					b.NL();
-					b.text("Estimated daily maintenance in denari: " + (int) maint_cost);
-					b.NL();
-///////////////////////////////////////////////////////////////////////////////////////////////////////
 				}
-				
-				
+
+
 				@Override
 				protected void clickA() {
 					if (g(get).upgrade() >= blueprint.upgrades().max()) {
@@ -279,18 +277,18 @@ final class ModuleUpgradable implements ModuleMaker {
 					for (int ri = 0; ri < blueprint.constructor().resources(); ri++) {
 						int am =  g(get).resAmount(ri, g(get).upgrade()+1)-g(get).resAmount(ri, g(get).upgrade());
 						if (am > 0) {
-							SETT.ROOMS().STOCKPILE.remove(blueprint.constructor().resource(ri), am, RTYPE.CONSTRUCTION);
+							blueprint.constructor().resource(ri).remove(am, RTYPE.CONSTRUCTION);
 						}
-						
+
 					}
 					g(get).upgradeSet(g(get).upgrade()+1);
 					super.clickA();
 				}
-				
+
 				@Override
 				protected void renAction() {
 					activeSet(true);
-					
+
 					if (g(get).upgrade() >= blueprint.upgrades().max()) {
 						activeSet(false);
 						return;
@@ -299,8 +297,8 @@ final class ModuleUpgradable implements ModuleMaker {
 						return;
 					else if (!g(get).blueprintI().upgrades().requires(g(get).upgrade()+1).passes(FACTIONS.player()))
 						activeSet(false);
-					
-					
+
+
 					for (int ri = 0; ri < blueprint.constructor().resources(); ri++) {
 						int am =  g(get).resAmount(ri, g(get).upgrade()+1)-g(get).resAmount(ri, g(get).upgrade());
 						if (am > SETT.ROOMS().STOCKPILE.tally().amountReservable.get(blueprint.constructor().resource(ri))) {
@@ -310,24 +308,30 @@ final class ModuleUpgradable implements ModuleMaker {
 					}
 				}
 			};
-			
-			b.add(new GText(UI.FONT().S, ¤¤UPGRADE).lablifySub(), 0, 0);
-			b.addRightC(16, new GStat() {
-				
+			b.body().setHeight(bb.body.height());
+			b.body().incrW(8);
+			b.addCentredY(new GText(UI.FONT().S, ¤¤UPGRADE).lablifySub(), 8);
+			b.addRightC(10, new GStat() {
+
 				@Override
 				public void update(GText text) {
 					GFORMAT.iofk(text, g(get).upgrade(), blueprint.upgrades().max());
 					text.lablify();
 				}
 			});
-			
+
 			b.body().incrW(64);
-			b.pad(8, 4);
-			
-			section.addRelBody(8, DIR.S,b);
-			
-			
-			
+
+			GuiSection s = new GuiSection();
+			s.add(b);
+
+
+			s.addRight(0, bb);
+
+			section.addRelBody(8, DIR.S, s);
+
+
+
 		}
 
 		private RoomInstance g(GETTER<RoomInstance> g) {
@@ -335,11 +339,33 @@ final class ModuleUpgradable implements ModuleMaker {
 		}
 
 
-		
+
 	}
 
-	
+	private void downGrade(RoomInstance ins) {
+		if (ins.upgrade() <= 0)
+			return;
+		ins.upgradeSet(ins.upgrade()-1);
+
+		for (COORDINATE c : ins.body()) {
+			if (ins.is(c) && SETT.MAINTENANCE().isser.is(c)) {
+				RESOURCE res = SETT.MAINTENANCE().resource.get(c);
+				if (res != null && !hasRes(ins, res)) {
+					SETT.MAINTENANCE().maintain(c.x(), c.y());
+				}
+			}
+		}
+	}
 
 
-	
+	private boolean hasRes(RoomInstance ins, RESOURCE res) {
+		for (int ri = 0; ri < ins.blueprintI().constructor().resources(); ri++) {
+			if (ins.blueprintI().constructor().resource(ri) == res) {
+				return ins.blueprintI().constructor().resourceHas(ri, ins.upgrade());
+			}
+		}
+		return false;
+	}
+
+
 }
