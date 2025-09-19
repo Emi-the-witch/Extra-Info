66aa402079b6a7ec6ac208148bdb85364cd30cd3
sleepyHolo <1665814195@qq.com>
2025-09-19 11:53:26 +0800

    add i18n for view.ui.goods.*

---
+++ src/main/java/util/dic/ExtraInfoDic.java
@@ -0,0 +1,202 @@
+package util.dic;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.Locale;
+import java.util.PropertyResourceBundle;
+import java.util.ResourceBundle;
+/////////////////////////////////////////////#!# This is a unique file that doesn't overwrite any of Jake's files.
+
+public class ExtraInfoDic {
+
+    /**
+     * replace all 'denari's with built-in localized string
+     * if a plural form is required, change here to Dic.¤¤Currs
+     */
+    public static CharSequence denari = Dic.¤¤Curr;
+    /**
+     * original: view.ui.goods.UIExpenses, line 130
+     * original: view.ui.goods.UIMaintenance, line 136
+     */
+    public static CharSequence totalCosts;
+
+
+    /**
+     * original: view.ui.goods.UIExpenses.¤¤Name
+     */
+    public static CharSequence expenses;
+    /**
+     * original: view.ui.goods.UIExpenses, line 41
+     */
+    public static CharSequence consumers;
+    /**
+     * original: view.ui.goods.UIExpenses, line 127
+     */
+    public static CharSequence titleExpenses;
+
+
+    /**
+     * original: view.ui.goods.UIIndustries.¤¤Name
+     */
+    public static CharSequence industries;
+    /**
+     * original: view.ui.goods.UIIndustries, line 41
+     */
+    public static CharSequence industryProfitability;
+
+
+    /**
+     * original: view.ui.goods.UIMaintenance.¤¤Name
+     */
+    public static CharSequence maintenance;
+    /**
+     * original: view.ui.goods.UIMaintenance, line 85
+     */
+    public static CharSequence overallMaintenance;
+    /**
+     * original: view.ui.goods.UIMaintenance, line 86
+     */
+    public static CharSequence titleMaintenance;
+    /**
+     * original: view.ui.goods.UIMaintenance, line 109
+     */
+    public static CharSequence overallBuildingMaintenance;
+    /**
+     * original: view.ui.goods.UIMaintenance, line 110
+     */
+    public static CharSequence titleBuildingMaintenance;
+    /**
+     * original: view.ui.goods.UIMaintenance, line 200
+     */
+    public static CharSequence total;
+
+
+    /**
+     * original: view.ui.goods.UIProduction.¤¤Name
+     */
+    public static CharSequence production;
+    /**
+     * original: view.ui.goods.UIProduction, line 41
+     */
+    public static CharSequence producers;
+    /**
+     * original: view.ui.goods.UIProduction, line 122
+     */
+    public static CharSequence titleProduction;
+    /**
+     * original: view.ui.goods.UIProduction, line 125
+     */
+    public static CharSequence totalValues;
+
+
+    /**
+     * original: view.ui.goods.UIRecipes.¤¤Name
+     */
+    public static CharSequence recipes;
+    /**
+     * original: view.ui.goods.UIRecipes, line 58
+     */
+    public static CharSequence titleRecipes;
+
+
+    /**
+     * original: view.ui.goods.UIValues.¤¤Name
+     */
+    public static CharSequence values;
+    /**
+     * original: view.ui.goods.UIValues, line 54
+     */
+    public static CharSequence titleValues;
+
+
+    static {
+        // not sure if this runs behind of main(String[] args), but it works (on my machine)
+        // if some lang could not use utf-8, edit later
+        loadProperties(
+                ResourceBundle.getBundle(
+                        "extra_info_i18n/extra_info", findLocale(), new EncodedControl(StandardCharsets.UTF_8)
+                )
+        );
+    }
+
+    /**
+     * this method is not always powerful. a much more stable way, visit:
+     * https://github.com/4rg0n/songs-of-syx-mod-more-options/blob/main/mod-sdk/src/main/java/com/github/argon/sos/mod/sdk/game/api/GameLangApi.java
+     */
+    private static Locale findLocale() {
+        if ("德纳里".contentEquals(Dic.¤¤Curr)) {
+            return Locale.SIMPLIFIED_CHINESE;
+        }
+        return Locale.ENGLISH;
+
+    }
+
+    private static void loadProperties(ResourceBundle bundle) {
+
+        totalCosts = bundle.getString("totalCosts");
+
+        // view.ui.goods.UIExpenses
+        expenses = bundle.getString("expenses");
+        consumers = bundle.getString("consumers");
+        titleExpenses = bundle.getString("titleExpenses");
+
+        // view.ui.goods.UIIndustries
+        industries = bundle.getString("industries");
+        industryProfitability = bundle.getString("industryProfitability");
+
+        // view.ui.goods.UIMaintenance
+        maintenance = bundle.getString("maintenance");
+        overallMaintenance = bundle.getString("overallMaintenance");
+        titleMaintenance = bundle.getString("titleMaintenance");
+        overallBuildingMaintenance = bundle.getString("overallBuildingMaintenance");
+        titleBuildingMaintenance = bundle.getString("titleBuildingMaintenance");
+        total = bundle.getString("total");
+
+        // view.ui.goods.UIProduction
+        production = bundle.getString("production");
+        producers = bundle.getString("producers");
+        titleProduction = bundle.getString("titleProduction");
+        totalValues = bundle.getString("totalValues");
+
+        // view.ui.goods.UIRecipes
+        recipes = bundle.getString("recipes");
+        titleRecipes = bundle.getString("titleRecipes");
+
+        // view.ui.goods.UIValues
+        values = bundle.getString("values");
+        titleValues = bundle.getString("titleValues");
+
+    }
+
+    private static class EncodedControl extends ResourceBundle.Control {
+        private final Charset charset;
+
+        EncodedControl(Charset charset) {
+            this.charset = charset;
+        }
+
+        @Override
+        public ResourceBundle newBundle(
+                String baseName, Locale locale, String format, ClassLoader loader, boolean reload
+        ) throws IOException {
+            String bundleName = toBundleName(baseName, locale);
+            String resourceName = toResourceName(bundleName, "properties");
+            try (InputStream stream = loader.getResourceAsStream(resourceName)) {
+                if (stream == null) {
+                    return null;
+                }
+                try (Reader reader = new InputStreamReader(stream, charset)) {
+                    return new PropertyResourceBundle(reader);
+                }
+            }
+        }
+
+
+    }
+
+
+}
